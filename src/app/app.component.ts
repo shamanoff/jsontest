3efb203158bba04473f495333ca7435765605598
Michael <shamanoff@gmail.com>
2017-05-09 07:53:28 +0300

    home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "./data-service.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ DataService ]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  ninjas = [];
+  bool = null;
+
+
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit(){
+    this.dataService.fetchData().subscribe(
+      // data => console.log(data)
+      (data) => this.ninjas = data
+    );
+  }
+
 }
---
+++ src/app/data-service.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {Http} from "@angular/http";
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DataService {
+
+  constructor(private http: Http) {
+  }
+
+  fetchData() {
+    return this.http.get('../assets/data.json').map(
+      (res) => res.json()
+    );
+
+  }
+}
